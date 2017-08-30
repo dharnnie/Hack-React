203b56e6b3f2fab9bb84d92b99a52c806b966fdd
Daniel Osineye <daniel.osineye@gmail.com>
2017-08-30 20:25:47 +0100

    States - Messing around

--- src/app/index.js
+++ src/app/index.js
@@ -7,6 +7,7 @@ import {Comments} from "./components/Comments";
 
 class Body extends React.Component {
   render(){
+    // these will change with time - dynamic data
     var user1 = {
       nick: "danny",
       comment: "I will refer to this folder as the business logic entry of the app. In go, folders are referred to as packages, and in every package you can have several dot go files (Go creators advice that you limit files in each package to 33.. Oh no! I mean 3). 33 files? Lol, except you are building Facebook + Google + Windows OS in one app."
