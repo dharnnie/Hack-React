ab0030ddd8026f0aec467d589f508fa0880f3f20
Daniel Osineye <daniel.osineye@gmail.com>
2017-09-05 17:55:52 +0100

    thingd

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -2,13 +2,16 @@ import React from "react";
 
 
 export class Header extends React.Component {
-  render() {
-    return(
-      <nav>
-          <div className="nav-wrapper grey">
-              <a href="#" className="center brand-logo">Organising your Go file!</a>
-          </div>
-      </nav>
-    );
-  }
+    render() {
+        return ( <
+            nav >
+            <
+            div className = "nav-wrapper grey" >
+            <
+            a href = "#"
+            className = "center brand-logo" > Organising your Go file! < /a> </div >
+            <
+            /nav>
+        );
+    }
 }
