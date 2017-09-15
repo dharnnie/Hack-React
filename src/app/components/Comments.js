82376b94fa575f8cf443057e0311d5ddbe9090e1
Daniel Osineye <daniel.osineye@gmail.com>
2017-09-15 01:34:06 +0100

    Drop a live comment

--- src/app/components/Comments.js
+++ src/app/components/Comments.js
@@ -1,22 +1,56 @@
 import React from "react";
 
 export class Comments extends React.Component {
-  
-  onComment(){
+  constructor(props){
+    super(props);
+    var initialComments = this.props.comms;
 
+    this.state = {
+      comments: initialComments,
+      clicks: 3,
+      n: "Me",
+      c: "My Comment"
+    }
+  }
+  nickChange = (evt) =>{
+    this.setState({
+      n:evt.target.value
+    });
+  }
+  commentChange = (evt) =>{
+    this.setState({
+      c: evt.target.value
+    });
+  }
+
+  doStuff(event){
+    this.setState({
+      clicks: this.state.clicks += 1
+    });
+  }
+
+  handleSubmit = (evt) =>{
+    evt.preventDefault();
+    var newComm = {
+      nick: this.state.n,
+      comment: this.state.c
+    }
+    this.setState({
+      comments: comments[comments.length] = newComm
+    });
   }
   render() {
     return(
       <div>
-        // LEAVE A COMMENT
+      <button type="submit" onClick={() => this.doStuff()}>clicker</button>
         <div className="">
             <div className="grey">
                 <ul className="collection grey">
-                    {this.props.comms.map(
+                    {this.state.comments.map(
                       (payload,i)=>
                       <li className="collection-item avatar" key={i}>
                           <i className="material-icons circle">create</i>
-                          <span className="title"><strong>{payload.nick}</strong></span>
+                          <span className="title"><strong>{payload.nick} - {this.state.clicks}</strong></span>
                           <p>
                               {payload.comment}
                           </p>
@@ -24,26 +58,33 @@ export class Comments extends React.Component {
                     )}
                 </ul>
             </div>
+            <ul className="collection grey" >
+            <li className="collection-item avatar">
+                <i className="material-icons circle">create</i>
+                <span className="title"><strong>{this.state.n} - {this.state.clicks}</strong></span>
+                <p>
+                    {this.state.c}
+                </p>
+            </li>
+            </ul>
         </div>
         <div className="row">
-            <form className="col s12" action="#">
+            <form onSubmit={this.handleSubmit}  className="col s12">
                 <div className="row">
                     <div className="input-field col s2">
-                        <input className="validate" id="nick" type="text" name="" value=""/>
+                        <input className="validate"  id="nick" type="text" onChange={this.nickChange} value={this.state.n} name="nick"/>
                         <label className="active" htmlFor="nick">Nick</label>
                     </div>
                 </div>
                 <div className="row">
                     <div className="input-field col s6">
-                        <textarea className="materialize-textarea" id="textarea1" name="name"></textarea>
+                        <textarea type="text" className="materialize-textarea" onChange={this.commentChange} id="textarea1" name="comment"></textarea>
                         <label className="active" htmlFor="textarea1">Leave a comment</label>
                     </div>
                 </div>
-                // BUTTON
                 <div className="row col s3">
-                    <button className="btn waves-effect waves-light">Submit!
-                      <i className="material-icons right">send</i>
-                    </button>
+                      <button className="btn waves-effect waves-light">Submit
+                      </button>
                 </div>
             </form>
         </div>
--- src/app/index.js
+++ src/app/index.js
@@ -1,39 +1,46 @@
-import React from "react";
-import {render} from "react-dom";
+import React, { Component } from "react";
+import { render } from "react-dom";
 
-import {Header} from "./components/Header";
-import {Post} from "./components/Post";
-import {Comments} from "./components/Comments";
+import { Header } from "./components/Header";
+import { Post } from "./components/Post";
+import { Comments } from "./components/Comments";
 
-class Body extends React.Component {
-  render(){
-    // these will change with time - dynamic data
-    var user1 = {
-      nick: "danny",
-      comment: "I will refer to this folder as the business logic entry of the app. In go, folders are referred to as packages, and in every package you can have several dot go files (Go creators advice that you limit files in each package to 33.. Oh no! I mean 3). 33 files? Lol, except you are building Facebook + Google + Windows OS in one app."
-    };
-    var user2 = {
-      nick: "odun",
-      comment: "ServeResource() serves all static files and this function was referenced in line 19 of the first screenshot. Don’t worry about how they work for now. I’ll try to make a video to explain this properly and update here."
-    };
-    var initialComms = [
-      user1,
-      user2
-    ];
-    return(
-      <div>
-        <div>
-          <Header/>
-        </div>
-        <div>
-          <Post/>
-        </div>
-        <div>
-          <Comments comms={initialComms} />
-        </div>
-      </div>
-    );
-  }
+class Body extends Component {
+    render() {
+        // these will change with time - dynamic data
+        var user1 = {
+            nick: "danny",
+            comment: "I will refer to this folder as the business logic entry of the app. In go, folders are referred to as packages, and in every package you can have several dot go files (Go creators advice that you limit files in each package to 33.. Oh no! I mean 3). 33 files? Lol, except you are building Facebook + Google + Windows OS in one app."
+        };
+        var user2 = {
+            nick: "odun",
+            comment: "ServeResource() serves all static files and this function was referenced in line 19 of the first screenshot. Don’t worry about how they work for now. I’ll try to make a video to explain this properly and update here."
+        };
+        var initComms = [
+            user1,
+            user2
+        ];
+        return ( <
+            div >
+            <
+            div >
+            <
+            Header / >
+            <
+            /div> <
+            div >
+            <
+            Post / >
+            <
+            /div> <
+            div >
+            <
+            Comments comms = { initComms }
+            /> < /
+            div > <
+            /div>
+        );
+    }
 }
 
-render(<Body />, document.getElementById("vlogpost"));
+render( < Body / > , document.getElementById("vlogpost"));
